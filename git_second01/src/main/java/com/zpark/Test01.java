3a11ad24a60e3250d2db8fe29a9901b67f78f01c
pch <good@123>
2023-10-26 22:25:59 +0800

    hot-fix commit handsome

--- git_second01/src/main/java/com/zpark/Test01.java
+++ git_second01/src/main/java/com/zpark/Test01.java
@@ -3,5 +3,8 @@ package com.zpark;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello nice");
+        System.out.println("hello boy");
+
     }
 }
