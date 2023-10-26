b0bdecb7d337a5da1fb46e1d561ebbed4227e7fe
pch <good@123>
2023-10-26 20:59:50 +0800

    second commit handsome

--- git_second01/src/main/java/com/zpark/Test01.java
+++ git_second01/src/main/java/com/zpark/Test01.java
@@ -3,5 +3,7 @@ package com.zpark;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
     }
 }
