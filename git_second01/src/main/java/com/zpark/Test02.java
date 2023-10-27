35c56a3f5731a1d22112f0490d8194bc50769fec
handsomepch <148631697+handsomepch@users.noreply.github.com>
2023-10-27 21:29:10 +0800

    pull   first  commit

--- git_second01/src/main/java/com/zpark/Test02.java
+++ git_second01/src/main/java/com/zpark/Test02.java
@@ -10,5 +10,8 @@ public class Test02 {
 
         System.out.println("请输入您的姓名：");
         String name = sc.next();
+
+     System.out.println("很高兴认识你，希望有机会再见");
+         System.out.println("不努力，一切与我无关，我努力，至少还有一丝丝希望！");
     }
 }
