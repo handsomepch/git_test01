d28fe71f020c86eeaa92fea8fedd0096c9333ea8
pch <good@123>
2023-10-27 21:06:38 +0800

    push commit

--- git_second01/src/main/java/com/zpark/Test01.java
+++ git_second01/src/main/java/com/zpark/Test01.java
@@ -1,20 +1,15 @@
 package com.zpark;
 
+import sun.security.krb5.SCDynamicStoreConfig;
+
+import java.util.Scanner;
+
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world1");
-        System.out.println("hello world2");
-        System.out.println("hello world3");
-        System.out.println("hello world4");
-        System.out.println("hello nice");
-        System.out.println("hello boy");
-        System.out.println("改变世界，改变自己");
-        System.out.println("hot-fix");
-
-        System.out.println("天行健，君子以自强不息");
-        System.out.println("master");
-
+        System.out.println("请输入你的名字");
+        Scanner sc=new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
 
     }
 }
